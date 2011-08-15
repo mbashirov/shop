c8a6c28593f34b80ba11d4a6c3e08d3ce6a15df3
esk <esk@esk-vb.(none)>
2011-08-15 22:58:28 +0400

    Empty rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shop::Application.config.secret_token = '195c89280cf128ff76f815423752e100e47c85a1be283d22aa6744d15e38be549586dd74d791323ff7c5d8addc0c7e5abea3a85e430654e670725565d8b82b25'
